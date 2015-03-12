e8568cdd10acce6101b008737376df7b567b0468
Matt Burch <mattdburch@gmail.com>
2015-03-12 16:49:48 -0500

    Add comments to the get file

--- get_tidydata.R
+++ get_tidydata.R
@@ -1,4 +1,8 @@
 library(dplyr)
+
+# Read the tidyData file back into R
 tidydata <- read.table("tidyData", header = TRUE) 
+# Convert to a data frame
 tidydata<- tbl_df(tidydata)
+# Display the contents of tidydata to the user
 View(tidydata)
