3792c2cec6e29d5120edbc926b5768f8509ee64b
Matt Burch <mattdburch@gmail.com>
2015-03-14 09:13:34 -0500

    Fixed paths so the scripts dont need to be in UCI HAR Dataset

--- get_tidydata.R
+++ get_tidydata.R
@@ -1,7 +1,9 @@
 library(dplyr)
 
+dir <- paste0(getwd(), "/UCI HAR Dataset")
+
 # Read the tidyData file back into R
-tidydata <- read.table("tidyData.txt", header = TRUE) 
+tidydata <- read.table(paste0(dir,"/tidyData.txt"), header = TRUE) 
 # Convert to a data frame
 tidydata<- tbl_df(tidydata)
 # Display the contents of tidydata to the user
--- run_analysis.R
+++ run_analysis.R
@@ -1,17 +1,19 @@
 library(dplyr)
 
+dir <- paste0(getwd(), "/UCI HAR Dataset")
+
 # Read in the test data
-testX <- read.table("./test/X_test.txt")
-testY <- read.table("./test/Y_test.txt")
-testSubject <- read.table("./test/subject_test.txt")
+testX <- read.table(paste0(dir,"/test/X_test.txt"))
+testY <- read.table(paste0(dir,"/test/Y_test.txt"))
+testSubject <- read.table(paste0(dir,"/test/subject_test.txt"))
 
 # Bind the test data together
 testData <- cbind(testSubject, testY, testX)
 
 # Read in the training data
-trainX <- read.table("./train/X_train.txt")
-trainY <- read.table("./train/Y_train.txt")
-trainSubject <- read.table("./train/subject_train.txt")
+trainX <- read.table(paste0(dir,"/train/X_train.txt"))
+trainY <- read.table(paste0(dir,"/train/Y_train.txt"))
+trainSubject <- read.table(paste0(dir,"/train/subject_train.txt"))
 
 # Bind the training data together
 trainData <- cbind(trainSubject, trainY, trainX)
@@ -23,13 +25,13 @@ fullData <- tbl_df(fullData)
 # Read in the column names
 # Modify the names so they can be applied as column names
 # Set names to columns names
-colNames <- read.table("./features.txt", stringsAsFactors=FALSE)$V2
+colNames <- read.table(paste0(dir,"/features.txt"), stringsAsFactors=FALSE)$V2
 colNames <- gsub("-", "_", colNames)
 colNames <- gsub("()", "Val", colNames, fixed=TRUE)
 colnames(fullData) <- c("subject", "activity", make.names(colNames, unique=TRUE))
 
 # Change the activity descriptions
-activity_labels <- read.table("./activity_labels.txt")$V2
+activity_labels <- read.table(paste0(dir,"/activity_labels.txt"))$V2
 fullData$activity <-as.factor(fullData$activity)
 levels(fullData$activity) <- activity_labels
 
